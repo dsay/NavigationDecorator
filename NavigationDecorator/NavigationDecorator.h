33f55896a9dfe2d44910e384d2dd5e5ec8aa3d12
dsay <dsay@ciklum.com>
2017-05-17 10:57:01 +0300

    Initial Commit

---
+++ NavigationDecorator/NavigationDecorator.h
@@ -0,0 +1,19 @@
+//
+//  NavigationDecorator.h
+//  NavigationDecorator
+//
+//  Created by User on 5/17/17.
+//  Copyright © 2017 Dima Sai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NavigationDecorator.
+FOUNDATION_EXPORT double NavigationDecoratorVersionNumber;
+
+//! Project version string for NavigationDecorator.
+FOUNDATION_EXPORT const unsigned char NavigationDecoratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NavigationDecorator/PublicHeader.h>
+
+
